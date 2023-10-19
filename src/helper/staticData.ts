f5881f407a836fc1641795c9f07f280595bbc1aa
Divya Gaurav <divyagaurav1602045@gmail.com>
2023-10-19 15:25:31 +0530

    add static section of footer or company card

---
+++ src/helper/staticData.ts
@@ -0,0 +1,92 @@
+// <li>
+// <a href="/">Home</a>
+// </li>
+// <li>
+// <a href="/services">Services</a>
+// </li>
+// <li>
+// <a href="/blog">Blog</a>
+// </li>
+// <li>
+// <a href="/work">Our Work</a>
+// </li>
+// <li>
+// <a href="/careers">Careers</a>
+// </li>
+// <li>
+// <a href="/about">About Us</a>
+// </li>
+
+export const companyImages = [
+    {
+        id: 1,
+        imgSrc: "https://neon.ipsator.com/c/image/upload/c_lpad,f_auto,h_100,w_200/v1586001930/ipsator_com/company_logos/irctc.png"
+    },
+    {
+        id: 2,
+        imgSrc: "https://neon.ipsator.com/c/image/upload/c_lpad,f_auto,h_100,w_200/v1586001930/ipsator_com/company_logos/razorpay.png"
+    },
+    {
+        id: 3,
+        imgSrc: "https://neon.ipsator.com/c/image/upload/c_lpad,f_auto,h_100,w_200/v1586001929/ipsator_com/company_logos/dominos.png"
+    },
+    {
+        id: 4,
+        imgSrc: "https://neon.ipsator.com/c/image/upload/b_rgb:ffffff,bo_20px_solid_rgb:ffffff,c_lpad,f_auto,h_60,w_160/v1586001929/ipsator_com/company_logos/faasos.png"
+    },
+    {
+        id: 5,
+        imgSrc: "https://neon.ipsator.com/c/image/upload/c_lpad,f_auto,h_100,w_200/v1586001930/ipsator_com/company_logos/wipro.png"
+    },
+    {
+        id: 6,
+        imgSrc: "https://neon.ipsator.com/c/image/upload/c_lpad,f_auto,h_100,w_200/v1586001930/ipsator_com/company_logos/ge.png"
+    },
+    {
+        id: 7,
+        imgSrc: "https://neon.ipsator.com/c/image/upload/b_rgb:ffffff,bo_30px_solid_rgb:ffffff,c_lpad,f_auto,h_40,w_140/v1586001929/ipsator_com/company_logos/paytm.png"
+    },
+    {
+        id: 8,
+        imgSrc: "https://neon.ipsator.com/c/image/upload/c_lpad,f_auto,h_100,w_200/v1586001929/ipsator_com/company_logos/mmt.png"
+    }
+]
+
+export const servicesData = [
+    {
+        id: 1,
+        imgSrc: "https://images.unsplash.com/photo-1532073145718-62df48eaa35e?auto=format&fit=crop&q=80&w=2668&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Strategy & Planning",
+        paragraph: "We work with you to find the right answers to your questions. Our strength lies in identifying the requirements from complex and ambiguous problems by holistic systems thinking approach.        "
+    },
+    {
+        id: 2,
+        imgSrc: "https://images.unsplash.com/photo-1556745753-b2904692b3cd?auto=format&fit=crop&q=80&w=2573&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Branding & Identity",
+        paragraph: "Every company is unique. Bringing it out in products and services is essential for connecting with desired target audiences. We will help you discover that essence and stand out from the crowd."
+    },
+    {
+        id: 3,
+        imgSrc: "https://images.unsplash.com/photo-1540612597331-63c67ea382fc?auto=format&fit=crop&q=80&w=2680&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "UI/UX Design",
+        paragraph: "We are user focussed with no exceptions. We use user-centred design methods to uncover insights that champion user experience. We then convert them into beautiful & functional UIs that users will love."
+    },
+    {
+        id: 4,
+        imgSrc: "https://images.unsplash.com/photo-1522125670776-3c7abb882bc2?auto=format&fit=crop&q=80&w=2670&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Mobile Development",
+        paragraph: "Our team has built apps used by millions of users. We create high-quality, well-tested, maintainable, and future-ready apps that you will love, and your users deserve."
+    },
+    {
+        id: 5,
+        imgSrc: "https://images.unsplash.com/photo-1519222970733-f546218fa6d7?auto=format&fit=crop&q=80&w=2670&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Web Development",
+        paragraph: "We believe content on the web should be accessible to all human beings on all networks. We stress heavily on web performance optimizations. We build single page apps to Progressive web apps."
+    },
+    {
+        id: 6,
+        imgSrc: "https://images.unsplash.com/photo-1571786256017-aee7a0c009b6?auto=format&fit=crop&q=80&w=2680&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Server Dev Dev-Ops",
+        paragraph: "Want to move fast while not breaking things? We help you with tooling, versioning and branching strategies, and confident releases. The solutions designed are scalable both horizontally and vertically."
+    },
+]
